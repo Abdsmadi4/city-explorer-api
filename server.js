3bbd3c5d0b6421c326a4d7eb7ec0296ec01ea85a
Abdallah Smadi <abd.smadi41@gmail.com>
2022-08-06 18:36:46 +0300

    lab08

--- server.js
+++ server.js
@@ -6,7 +6,7 @@
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
-
+const axios = require('axios');
 
 
 const app = express();
@@ -16,19 +16,30 @@ let PORT = process.env.PORT || 3001;
 const weatherData = require('./data/weather.json');
 
 
-//Routes
+//Routes , endpoints
 app.get('/weather', handleWeather);
+
+app.get('/movies', handleMovies);
+
+app.get('*', (req, res) => {
+  res.status(404).send('Page not found');
+});
+
+
 
 
 //Functions
-function handleWeather(req, res) {
+async function handleWeather(req, res) {
 
   const { searchQuery, lat, lon } = req.query;
 
-  let cityArr = weatherData.find(value => value.city_name.toLowerCase() === searchQuery.toLowerCase());
+  // const cityArr = weatherData.find(value => value.city_name.toLowerCase() === searchQuery.toLowerCase());
+  const url = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${process.env.WEATHER_API_KEY}&include=minutely`;
 
+
+  const cityArr = await axios.get(url);
   try {
-    const cityData = cityArr.data.map(value => new Forecast(value));
+    const cityData = cityArr.data.data.map(value => new Forecast(value));
     res.status(200).send(cityData);
   } catch (error) {
     errorHandlre(error, res);
@@ -36,19 +47,41 @@ function handleWeather(req, res) {
 
 }
 
-app.get('*', (req, res) => {
-  res.status(404).send('Page not found');
-});
+async function handleMovies(req, res) {
+  const { searchQuery } = req.query;
+  const url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${searchQuery}`;
+  const movieArr = await axios.get(url);
+
+  try {
+    const movieData = movieArr.data.results.map(item => new Movie(item));
+    res.status(200).send(movieData);
+  } catch (error) {
+    errorHandlre(error, res);
+  }
+
+}
 
 
-function errorHandlre (error, res) {
+function errorHandlre(error, res) {
   res.status(500).send({ error: error.responce.data.error });
 }
 
 class Forecast {
   constructor(day) {
     this.description = day.weather.description;
-    this.date = day.datetime;
+    this.date = day.valid_date;
+  }
+}
+
+class Movie {
+  constructor(movie) {
+    this.title = movie.title;
+    this.overview = movie.overview;
+    this.average_votes = movie.vote_average;
+    this.total_votes = movie.vote_count;
+    this.image_url = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+    this.popularity = movie.popularity;
+    this.released_on = movie.release_date;
   }
 }
 
